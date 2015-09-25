cace99fa3e4c6c62f14ccfd6a67755d7b5fafb37
Jeffrey Harrington <jeff.harrington@gmail.com>
2015-09-24 21:20:28 -0500

    Styling and JS

--- config.js
+++ config.js
@@ -1,15 +1,13 @@
 module.exports = {
-    twitter: 
-        {
-              consumer_key:         ''
-            , consumer_secret:      ''
-            , access_token:         ''
-            , access_token_secret:  ''
-         },
-     particle: 
-     {
-           enabled: false
-         , device_id: ''
-         , access_token: ''
-     }
+  twitter: {
+    consumer_key: 'kDR5jdM323W8nqDr1IKpvErwI',
+    consumer_secret: 'E6KyyBIQTRNbQZlMJRAEot5nApa6X6nDiibojYfNUik64HRzEV',
+    access_token: '614549876-ePd0jkRmRXq3VBBS0cL653vfexqpxhpdj7EZUyMW',
+    access_token_secret: 't72fJ7Vzg5IhDA7kFTppW0x4l4B9RpTjhtwpA4yDqBIYH'
+  },
+  particle: {
+    enabled: false,
+    device_id: '',
+    access_token: ''
+  }
 };
